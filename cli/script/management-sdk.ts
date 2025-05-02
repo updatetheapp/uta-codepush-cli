315a14aac0ab81baa9b63057d75b96aeb1e12254
Phuong To <phuongwd@gmail.com>
2025-05-02 10:40:05 +0700

    Update SERVER_URL in AccountManager to use production API endpoint

--- cli/script/management-sdk.ts
+++ cli/script/management-sdk.ts
@@ -58,7 +58,7 @@ class AccountManager {
     OWNER: "Owner",
     COLLABORATOR: "Collaborator",
   };
-  public static SERVER_URL = "http://localhost:3000";
+  public static SERVER_URL = "https://updatetheapp.com/api";
 
   private static API_VERSION: number = 2;
 
