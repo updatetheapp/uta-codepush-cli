7e7b64f1f0b6ab782ea1d34d37129340ef2cbc50
Phuong To <phuongwd@gmail.com>
2025-05-02 12:42:51 +0700

    Remove deprecated files and configurations from the API
    - Deleted unused configuration files including .env.example, .eslintrc.json, and .deployment.
    - Removed infrastructure and deployment scripts such as codepush-infrastructure.bicep and parameters.json.
    - Cleaned up various utility and script files that are no longer in use, including routes and views.
    - This cleanup helps streamline the codebase and eliminate unnecessary clutter.

--- api/prettier.config.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-module.exports = {
-  printWidth: 135,
-  endOfLine: "auto",
-};
--- api/script/environment.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export function getTempDirectory(): string {
-  return process.env.TEMP || process.env.TMPDIR;
-}
